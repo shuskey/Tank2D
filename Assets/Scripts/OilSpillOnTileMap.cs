a5d11060c7a0740c144ce19bdc3f507903405c7b
Scott Huskey <scott@photoloom.com>
2022-10-31 17:32:11 -0700

    Oil spill on tank kill

---
+++ Assets/Scripts/OilSpillOnTileMap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OilSpillOnTileMap : MonoBehaviour
+{
+    [SerializeReference] private Tile oilSpillTile;
+    private string groundOverlayTilemapName = "Ground Overlay";
+    private string inFrontOfPlayerTilemapName = "In Front of Player";
+
+    // Update is called once per frame
+    public void DropAnOilSpill()
+    {      
+         var tankAssetTransform = transform.Find("Canvas");
+
+        Tilemap tilemap = GameObject.FindObjectsOfType<Tilemap>().Where<Tilemap>(i => i.name == inFrontOfPlayerTilemapName).FirstOrDefault();
+        var currentTileCellCoordinates = tilemap.WorldToCell(tankAssetTransform.position);
+        tilemap.SetTile(currentTileCellCoordinates, null);  // delete anything OVER the player
+
+        tilemap = GameObject.FindObjectsOfType<Tilemap>().Where<Tilemap>(i => i.name == groundOverlayTilemapName).FirstOrDefault();
+        currentTileCellCoordinates = tilemap.WorldToCell(tankAssetTransform.position);
+        tilemap.SetTile(currentTileCellCoordinates, oilSpillTile);
+        
+        
+    }
+}
